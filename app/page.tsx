967a984a2ddb275def285330a11d5ce7386831ad
Vaibhav Barman <vaibhavbarman@Vaibhavs-MacBook-Air.local>
2026-07-01 00:42:42 +0530

    Created Reusable navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Navbar from "@/components/layout/Navbar";
+
 export default function Home() {
   return (
     <main>
+      <Navbar />
+
       <h1>Moneylect</h1>
     </main>
   );
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Moneylect</h2>
+    </nav>
+  );
+}
